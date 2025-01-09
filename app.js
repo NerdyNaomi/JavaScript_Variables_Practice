ee5b1d1e26bd1f6ad231da74be2d317a6f3cb9f6
Naomi Weneck <naomiweneck@gmail.com>
2025-01-08 18:56:09 -0700

    Completed Assignment

--- app.js
+++ app.js
@@ -4,7 +4,40 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Naomi";
+let lastName = "Last Name";
+var age = 0;
 
 // Exercise 2
+lastName = "Weneck";
+age = 28;
+
+console.log(`My name is ${firstName} ${lastName} and I am ${age} years old.`)
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995
+let isCaseSensitive = true;
+
+console.log(`${language} was first released in ${createdYear}.`)
+
+//Exercise 4
+let product = "Anime T-Shirt"
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "Medium";
+
+console.log(`The ${product} in size ${selectedSize} is $${price} and there are ${stock} ${selectedSize} ${product}s available.`)
+
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`You are on page ${bookmark} of ${pageCount} pages of ${title} by ${author}. You have ${pageCount - bookmark} pages left to read.`)
